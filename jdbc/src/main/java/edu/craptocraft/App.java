6d98158477e672dff0856e2c22a1a427e74cec58
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-03-31 17:10:27 +0200

    feat(databaseConnection)

--- jdbc/src/main/java/edu/craptocraft/App.java
+++ jdbc/src/main/java/edu/craptocraft/App.java
@@ -1,13 +1,39 @@
 package edu.craptocraft;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class App 
 {
-    public static void main( String[] args )
+
+    private static Connection connection;
+
+
+
+    public static void main( String[] args ) throws SQLException
     {
-        System.out.println( "Hello World!" );
+        openDataBaseConnection();
+
+        closeDataBaseConnection();
+
+    }
+
+
+    public static void openDataBaseConnection() throws SQLException{
+        System.out.println("Connecting to the database...");
+        connection = DriverManager.getConnection(
+        "jdbc:mariadb://localhost:3306/biblioteca_JDBC",
+        "user", "1234567890"
+        );
+        System.out.println("Connection valid: "+ connection.isValid(5));
+        
+    }
+
+    public static void closeDataBaseConnection() throws SQLException{
+        System.out.println("Closing database connection");
+        connection.close();
+        System.out.println("Connection valid: "+ connection.isValid(5));
     }
 }
