8227e666da0528cb3ddb2fdd21ef50b145acbb8a
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-03-31 17:48:00 +0200

    try catch in main program

--- jdbc/src/main/java/edu/craptocraft/App.java
+++ jdbc/src/main/java/edu/craptocraft/App.java
@@ -4,14 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.management.RuntimeErrorException;
+
 public class App {
 
     private static Connection connection;
 
     public static void main(String[] args) throws SQLException {
-        openDataBaseConnection();
+        try {
 
-        closeDataBaseConnection();
+            openDataBaseConnection();
+
+        } finally {
+            closeDataBaseConnection();
+        }
 
     }
 
@@ -21,7 +27,8 @@ public class App {
                 "jdbc:mariadb://localhost:3306/biblioteca_JDBC",
                 "root", "1234567890");
         System.out.println("Connection valid: " + connection.isValid(5));
-
+        if (true)
+            throw new RuntimeErrorException(null, "Simulated error!");
     }
 
     public static void closeDataBaseConnection() throws SQLException {
