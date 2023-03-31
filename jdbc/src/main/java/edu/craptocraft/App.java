3b074d6a9b104a884f195741e4e0623142767c62
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-03-31 17:40:41 +0200

    fix(bug)

--- jdbc/src/main/java/edu/craptocraft/App.java
+++ jdbc/src/main/java/edu/craptocraft/App.java
@@ -4,36 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
-public class App 
-{
+public class App {
 
     private static Connection connection;
 
-
-
-    public static void main( String[] args ) throws SQLException
-    {
+    public static void main(String[] args) throws SQLException {
         openDataBaseConnection();
 
         closeDataBaseConnection();
 
     }
 
-
-    public static void openDataBaseConnection() throws SQLException{
+    public static void openDataBaseConnection() throws SQLException {
         System.out.println("Connecting to the database...");
         connection = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/biblioteca_JDBC",
-        "user", "1234567890"
-        );
-        System.out.println("Connection valid: "+ connection.isValid(5));
-        
+                "jdbc:mariadb://localhost:3306/biblioteca_JDBC",
+                "root", "1234567890");
+        System.out.println("Connection valid: " + connection.isValid(5));
+
     }
 
-    public static void closeDataBaseConnection() throws SQLException{
+    public static void closeDataBaseConnection() throws SQLException {
         System.out.println("Closing database connection");
         connection.close();
-        System.out.println("Connection valid: "+ connection.isValid(5));
+        System.out.println("Connection valid: " + connection.isValid(5));
     }
 }
