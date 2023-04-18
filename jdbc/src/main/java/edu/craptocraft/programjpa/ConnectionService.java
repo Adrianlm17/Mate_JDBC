acc4f230ef1ad4add5f7929c6547dac3a5555dfb
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-04-18 20:16:47 +0200

    feat(jpa dir)

---
+++ jdbc/src/main/java/edu/craptocraft/programjpa/ConnectionService.java
@@ -0,0 +1,41 @@
+package edu.craptocraft.programjpa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionService {
+
+    public static Connection globalConnection;
+
+    public static Connection openDataBaseConnection() throws SQLException {
+
+        System.out.println("Connecting to the database...");
+        globalConnection = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/biblioteca_JDBC",
+                "root", "1234567890");
+
+        System.out.println("Connection valid: " + globalConnection.isValid(5));
+
+        return globalConnection;
+        // if (true)
+        // throw new RuntimeErrorException(null, "Simulated error!");
+    }
+
+    public static void closeDataBaseConnection(Connection connection) throws SQLException {
+
+        System.out.println("Closing database connection");
+
+        connection.close();
+
+        System.out.println("Connection valid: " + connection.isValid(5));
+    }
+
+    public static Connection getConnection() {
+        return globalConnection;
+    }
+
+    public static void setConnection(Connection connection) {
+        ConnectionService.globalConnection = connection;
+    }
+}
